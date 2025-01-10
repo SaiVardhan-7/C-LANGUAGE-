b2c5e5e388047ec802c83a0b57d43b936e4205ff
SaiVardhan-7 <kattasaivardhan838@gmail.com>
2025-01-10 20:40:31 +0530

    Update hollowrect.c

--- Loops/hollowrect.c
+++ Loops/hollowrect.c
@@ -7,11 +7,11 @@ int main(){
    * * * * * *                     */
    for(int i=1;i<=4;i++){
        for(int j=1;j<=6;j++){ 
-          if(i==1 || j==1 || i==4 || j==4){
+          if(i==1 || j==1 || i==4 || j==6){
             printf("* ");
           }
           else{
-            printf("    ");
+            printf("  ");
           }
        }
        printf("\n");
